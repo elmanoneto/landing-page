e33c497350514af8c82f954c3557fdab91973fa9
Elmano Neto <elmanoneto@MacBook-Pro.local>
2020-12-01 22:27:14 -0300

    fetch data from api and add anchor link

---
+++ src/index.js
@@ -0,0 +1,43 @@
+
+function goOpenJobs() {
+    document.getElementById('open-jobs').scrollIntoView({
+        behavior: 'smooth'
+    })
+}
+
+async function getData() {
+    try {
+        const request = await fetch('http://www.mocky.io/v2/5d6fb6b1310000f89166087b')
+        const result = await request.json()
+        
+        const { vagas } = result
+
+        const handleData = 
+            vagas.map(vaga => {
+                if (!vaga.localizacao) {
+                    return { ...vaga, localizacao: 'Remoto' }
+                }
+
+                return vaga
+                }).filter(vaga => vaga.ativa)
+        
+        const element = document.getElementById('jobs-list')
+
+        handleData.forEach(job => {
+            element.innerHTML += `
+            <li>
+                <div class="description">
+                    <a href=${job.link} target="_blank">${job.cargo}</a>
+                </div>
+                <div class="location">
+                    ${job.localizacao === 'Remoto' ? job.localizacao : `${job.localizacao.bairro} - ${job.localizacao.cidade}, ${job.localizacao.pais}`}
+                </div>
+            </li>
+            `
+        });        
+    } catch (error) {
+        console.log(`Error: ${error}`)
+    }
+}
+
+getData()
